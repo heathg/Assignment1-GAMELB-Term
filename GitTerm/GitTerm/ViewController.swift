99c2f8c6002ffdf2d7ca9fd6b377eece6a6481bf
heathg <Heathglover@Heaths-MacBook-Pro.local>
2015-04-20 16:40:23 +1000

    uploading

---
+++ GitTerm/GitTerm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTerm
+//
+//  Created by Heath Glover on 20/04/2015.
+//  Copyright (c) 2015 HeathG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
